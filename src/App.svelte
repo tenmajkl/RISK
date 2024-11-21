97a511f4f8f72d3645a67c0f87092ee7f67c97a9
TENMAJKL <silder.hologram@gmail.com>
2024-11-21 23:28:37 +0000

    init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Question from "./lib/Question.svelte";
+    import questions from "./questions.json";
+
+</script>
+
+<main class="flex flex-col w-screen h-screen items-center p-5 text-white">
+<span class="text-5xl">RISKUJ!</span>
+<div class="grid grid-rows-6 grid-flow-col gap-3 grid-cols-2">
+    {#each questions as question}
+        <div class="flex items-center justify-center text-3xl p-2">{question.name}</div>
+        {#each question.values as [text, anwser, used], index}
+            <Question {text} {anwser} {index} {used}></Question>
+        {/each}
+    {/each}
+    </div>
+</main>
+
---
+++ src/lib/Question.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let text;
+    export let anwser
+    export let index;
+    export let used;
+
+    function click() 
+    {
+        if (used) {
+            return;
+        }
+        used = true;
+        // popup srandy jdu spat
+    }
+</script>
+
+<div class="flex items-center justify-center text-4xl shadow-xl p-5 {used ? 'bg-stone-300 text-stone-400' : 'text-[#973350] bg-white'}" on:click={click}>
+    {(index + 1) * 100}
+</div>
